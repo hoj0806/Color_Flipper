6e6abcb13c4390f496d1183d5a5fe2f9746de3ed
hongjaehoun <ehswkfn123@naver.com>
2023-10-07 16:07:33 +0900

    버그수정

--- script/main.js
+++ script/main.js
@@ -6,26 +6,23 @@ let body = document.body
 let alertBox = document.querySelector('.alert-box')
 let SimpleColorArr = ["Red", "Orange", "Blue", "Green" , "Yellow", "Navy", "Purple", "Hotpink", "Pink",  "Gray",  "Aqua", "Aquamarine", "Coral", "Greenyellow", "Indigo" , "Magenta", "Plum", "Violet" ]
 
-let changeRandomSimpleColor = () => {
-    let removeDuplicateSimpleArr = SimpleColorArr.filter((element) => element !== colorName.innerHTML)
+simpleBtn.addEventListener('click', function() {
+    alertBox.style.display = "none";
+    changeBtn.addEventListener('click', function() {
+        let removeDuplicateSimpleArr = SimpleColorArr.filter((element) => element !== colorName.innerHTML)
         let randomSimpleColor = removeDuplicateSimpleArr[Math.floor(Math.random() * removeDuplicateSimpleArr.length )]
         colorName.innerHTML = randomSimpleColor;
         body.style.backgroundColor = randomSimpleColor
-    }
-
-let changeRandomHexColor = () => {
-    let randomHexColor =   "#" + Math.floor(Math.random()*16777215).toString(16);
-    colorName.innerHTML = randomHexColor.toUpperCase();
-    body.style.backgroundColor = randomHexColor
-} 
-
-simpleBtn.addEventListener('click', function() {
-    alertBox.style.display = "none";
-    changeBtn.addEventListener('click', changeRandomSimpleColor)
-    
+        console.log(removeDuplicateSimpleArr)
+    })
 })
 
 hexBtn.addEventListener('click', function() {
     alertBox.style.display = 'none';
-    changeBtn.addEventListener('click', changeRandomHexColor)
+    changeBtn.addEventListener('click', function() {
+        let randomHexColor =   "#" + Math.floor(Math.random()*16777215).toString(16);
+        colorName.innerHTML = randomHexColor.toUpperCase();
+        body.style.backgroundColor = randomHexColor
+        console.log("hex")
+    })
 })
